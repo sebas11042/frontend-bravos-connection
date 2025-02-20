4769ba30f4fefcf9bdf6b1876906ec385634f7d5
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-02-20 16:59:22 -0600

    feat: mx league

---
+++ src/app/leagues/mx-league/page.tsx
@@ -0,0 +1,11 @@
+import MxLeaguePage from "@/app/leagues/components/mxLeague";
+
+const Page = () => {
+  return (
+    <div className="w-full h-screen">
+      <MxLeaguePage />
+    </div>
+  );
+};
+
+export default Page;
