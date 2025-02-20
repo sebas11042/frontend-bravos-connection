db49a6f65497fbdadb7202cb3fb2683a2dbc16cc
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-02-20 16:49:54 -0600

    feat: create ligue one page

---
+++ src/app/leagues/page.tsx
@@ -0,0 +1,11 @@
+import LigueOnePage from "@/app/leagues/components/ligueOne";
+
+const Page = () => {
+  return (
+    <div className="w-full h-screen">
+      <LigueOnePage />
+    </div>
+  );
+};
+
+export default Page;
